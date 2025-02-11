289275ffa8c912d5aca0f1425748a1057e9dcf26
Al Amin <alamin@Als-MacBook-Air.local>
2025-02-12 02:50:39 +0600

    first commit

---
+++ for-of.js
@@ -0,0 +1,24 @@
+const numbers = [12, 98, 45, 63, 21, 72];
+
+console.log(numbers);
+
+// for (const num of numbers) {
+//     console.log(num);
+// }
+
+
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruits', 'watermelon'];
+
+// for (){
+
+// }
+
+for (const fruit of fruits) {
+    console.log(fruit)
+    console.log('I want to eat')
+}
+
+for (const num of numbers) {
+    console.log(num);
+}
