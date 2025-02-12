dc552e89aa3ffc16bc914a4db64f551158af8ac3
Al Amin <alamin@Als-MacBook-Air.local>
2025-02-12 20:24:45 +0600

    added more loop examples

---
+++ break.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 15; i++){
+    console.log(i);
+    if (i >= 5) {
+        break;
+    }
+}
+
+console.log('life after breakup');
+
+let n = 54;
+while (n > 50) {
+    console.log(n);
+    n--;
+}
